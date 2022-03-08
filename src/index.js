8c67438d8f1493cedd02f8b0b7293363b6742afc
Andrew Smith <ajs3993@gmail.com>
2022-03-08 14:02:45 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './less/index.less'
 
 // Your code goes here!
+const BusPic = document.querySelector(".busPic");
+
+BusPic.onclick = function(evt) {
+    console.log("On click");
+  }
+
+  BusPic.addEventListener("mouseover", myScript);
+
+  function myScript(){
+      console.log('hi')
+  }
